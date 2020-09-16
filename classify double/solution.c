00e909dade494b0f2ffc24f0b273360f7edd2ab9
uzeaeng <uzeaeng@gmail.com>
2020-09-16 13:44:13 +0500

    added solution for the first hw

--- classify double/solution.c
+++ classify double/solution.c
@@ -10,61 +10,110 @@
  * You should use them in the main sections.
  */
 
-uint64_t convertToUint64 (double number) {
-    return *((uint64_t *)(&number));
+uint64_t convertToUint64(double number) {
+    return *((uint64_t*)(&number));
 }
 
-bool getBit (const uint64_t number, const uint8_t index) {
-    /// Your code here...
+bool getBit(const uint64_t number, const uint8_t index) {
+    uint64_t mask = 1ll << index;
+    uint64_t temp = mask & number;
+    temp >>= index;
+    return temp;
 }
 
+bool isExponentZero(const uint64_t number)
+{
+    for (int i = 62; i >= 52; i--)
+    {
+        if (getBit(number, i)) { return false; }
+    }
+    return true;
+}
+
+bool isExponentAllOnes(const uint64_t number)
+{
+    for (int i = 62; i >= 52; i--)
+    {
+        if (!getBit(number, i)) { return false; }
+    }
+    return true;
+}
+
+bool isFractionZero(const uint64_t number)
+{
+    for (int i = 51; i >= 0; i--)
+    {
+        if (getBit(number, i)) { return false; }
+    }
+    return true;
+}
+
+bool getSign(const uint64_t number)
+{
+    return getBit(number, 63);
+}
+
+bool isDenormalized(const uint64_t number)
+{
+    return isExponentZero(number) && getBit(number, 0);
+}
+
+bool isNormalized(const uint64_t number)
+{
+    return !isExponentZero(number) && !isExponentAllOnes(number);
+}
+
+bool isInfinity(const uint64_t number)
+{
+    return isExponentAllOnes(number) && isFractionZero(number);
+}
 
 /**
  * Checkers here:
  */
 
-bool checkForPlusZero (uint64_t number) {
-    /// Your code here.
+bool checkForPlusZero(uint64_t number) {
+    return number == 0ll;
 }
 
-bool checkForMinusZero (uint64_t number) {
+bool checkForMinusZero(uint64_t number) {
     return number == 0x8000000000000000;
 }
 
-bool checkForPlusInf (uint64_t number) {
-    /// Your code here.
+bool checkForPlusInf(uint64_t number) {
+    return isInfinity(number) && !getSign(number);
 }
 
-bool checkForMinusInf (uint64_t number) {
-    /// Your code here.
+bool checkForMinusInf(uint64_t number) {
+    return isInfinity(number) && getSign(number);
 }
 
-bool checkForPlusNormal (uint64_t number) {
-    /// Your code here.
+bool checkForPlusNormal(uint64_t number) {
+    return isNormalized(number) && !getSign(number);
 }
 
-bool checkForMinusNormal (uint64_t number) {
-    /// Your code here.
+bool checkForMinusNormal(uint64_t number) {
+    return isNormalized(number) && getSign(number);
 }
 
-bool checkForPlusDenormal (uint64_t number) {
-    /// Your code here.
+bool checkForPlusDenormal(uint64_t number) {
+    return isDenormalized(number) && !getSign(number);
 }
 
-bool checkForMinusDenormal (uint64_t number) {
-    /// Your code here.
+bool checkForMinusDenormal(uint64_t number) {
+    return isDenormalized(number) && getSign(number);
 }
 
-bool checkForSignalingNan (uint64_t number) {
-    /// Your code here.
+bool checkForSignalingNan(uint64_t number) {
+    return getSign(number) && isExponentAllOnes(number) && !getBit(number,51) && getBit(number, 0);
 }
 
-bool checkForQuietNan (uint64_t number) {
-    /// Your code here.
+bool checkForQuietNan(uint64_t number) {
+    return getSign(number) && isExponentAllOnes(number) && getBit(number, 51);
 }
 
 
-void classify (double number) {
+void classify(double number) {
     if (checkForPlusZero(convertToUint64(number))) {
         printf("Plus zero\n");
     }
