58c4f069e9f3a0526997dc06187c5389d86568b1
Liliya Makhmutova <uzeaeng@gmail.com>
2020-09-27 13:34:09 +0500

    hw2 1

--- count_det/src/main.s
+++ count_det/src/main.s
@@ -1,2 +1,46 @@
-/// Your code goes here...
+.text
+.global count_det
 
+count_det:
+  mov r1, #1
+  mov r2, #1
+  mov r3, #1
+  mov r4, #1
+  mov r5, #1
+  mov r6, #1
+  
+  ldr r8, [r0], #4  
+  mul r1, r8
+  mul r5, r8
+  ldr r8, [r0], #4  
+  mul r3, r8
+  mul r6, r8
+  ldr r8, [r0], #4 
+  mul r2, r8
+  mul r4, r8
+  ldr r8, [r0], #4  
+  mul r2, r8
+  mul r6, r8
+  ldr r8, [r0], #4 
+  mul r1, r8
+  mul r4, r8
+  ldr r8, [r0], #4  
+  mul r3, r8
+  mul r5, r8
+  ldr r8, [r0], #4  
+  mul r3, r8
+  mul r4, r8
+  ldr r8, [r0], #4  
+  mul r2, r8
+  mul r5, r8  
+  ldr r8, [r0], #4  
+  mul r1, r8
+  mul r6, r8  
+  mov r0, r1
+  add r0, r2
+  add r0, r3
+  sub r0, r4
+  sub r0, r5
+  sub r0, r6
+  
+  bx lr
