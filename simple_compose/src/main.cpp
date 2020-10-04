bec366ff17accb6338266cc886eb6374c6f7587c
Liliya Makhmutova <uzeaeng@gmail.com>
2020-10-04 12:33:59 +0500

    hw_compose 1

--- simple_compose/src/main.cpp
+++ simple_compose/src/main.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
 #include <functional>
 
-typedef std::function<int (int)> Op;
+typedef std::function<int(int)> Op;
 
-
-
-Op compose (size_t n, Op ops[]) {
-    /// Your code goes here.
+Op compose(size_t n, Op ops[]) {
+    return [n, ops](int x) -> int 
+    {
+        int val = x;
+        for (int i = n - 1; i >= 0; i--) 
+        {
+            val = (ops[i])(val); 
+        }
+        return val;
+    };
 }
 
 
-int main () {
+int main() {
     /// Simple tests:
 
     Op op1 =
-        [] (int x) -> int {
-            return x + 1;
-        };
+        [](int x) -> int {
+        return x + 1;
+    };
 
     auto op2 =
-        [] (int p) -> Op {
-            return [p] (int x) {
-                return p * x;
-            };
+        [](int p) -> Op {
+        return [p](int x) {
+            return p * x;
         };
+    };
 
     {
-        Op ops[4] = {op1, op2(2), op1, op2(2)};
+        Op ops[4] = { op1, op2(2), op1, op2(2) };
         if (compose(4, ops)(2) != 11) {
             std::cout << "FAILED AT TEST 1" << std::endl;
             return 0;
@@ -34,7 +40,7 @@ int main () {
     }
 
     {
-        Op ops[8] = {op1, op2(2), op1, op1, op1, op2(2), op2(2), op1};
+        Op ops[8] = { op1, op2(2), op1, op1, op1, op2(2), op2(2), op1 };
         if (compose(8, ops)(5) != 55) {
             std::cout << "FAILED AT TEST 2" << std::endl;
             return 0;
@@ -42,7 +48,7 @@ int main () {
     }
 
     {
-        Op ops[1] = {op1};
+        Op ops[1] = { op1 };
         if (compose(1, ops)(3) != 4) {
             std::cout << "FAILED AT TEST 3" << std::endl;
             return 0;
@@ -58,7 +64,7 @@ int main () {
     }
 
     {
-        Op ops[4] = {op2(2), op2(3), op2(4), op2(5)};
+        Op ops[4] = { op2(2), op2(3), op2(4), op2(5) };
         if (compose(4, ops)(1) != 120) {
             std::cout << "FAILED AT TEST 5" << std::endl;
             return 0;
